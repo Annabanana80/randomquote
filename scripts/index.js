a30562b788457a8470591068c42035eac1fd6332
Anna Sanderson <anna.sanderson@csuglobal.edu>
2017-10-26 22:08:38 -0400

    The original API shut down. Had to recode the project

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,47 @@
-var url = "https://random-quote-generator.herokuapp.com/api/quotes/random";
-var newQuote = function(json) {
-  $("#quote").html(JSON.stringify(json.quote));
-  $(".blockquote-footer").html(JSON.stringify(json.author).replace(/\"/g, ""));
-};
-$('#twitter').click(function(){
-      window.open('https://twitter.com/intent/tweet?text='+ $('#quote').html() + $('.blockquote-footer').html());;
-    });
-$(document).ready(function() {
-  $.getJSON(url, newQuote, "json");
+// var url = "http://random-quote-generator.herokuapp.com/api/quotes/random";
+// var newQuote = function(json) {
+//   $("#quote").html(JSON.stringify(json.quote));
+//   $(".blockquote-footer").html(JSON.stringify(json.author).replace(/\"/g, ""));
+// };
+// $('#twitter').click(function(){
+//       window.open('https://twitter.com/intent/tweet?text='+ $('#quote').html() + $('.blockquote-footer').html());;
+//     });
+// $(document).ready(function() {
+//   $.getJSON(url, newQuote, "json");
   
 
-  $("#getQuote").click(function() {
-    $.getJSON(url, newQuote, "json");
+//   $("#getQuote").click(function() {
+//     $.getJSON(url, newQuote, "json");
+//     var randomColor='rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
+//     $('body').css('background-color',randomColor);
+//     $('h1').css('color',randomColor);
+//     $('p').css('color',randomColor);
+//     $('#quote').css('color',randomColor);
+//     $('#author').css('color',randomColor);
+//     $('button').css('background-color',randomColor);
+  
+
+//   });
+// });
+//Had to rewrite as the herokuapp was shut down
+$(document).ready(function(){
+  function randQuote(){
+    $.ajax({
+      url:'https://andruxnet-random-famous-quotes.p.mashape.com/',
+      type: 'POST',
+      dataType: 'json',
+      success:function(json){
+        $("#quote").html(json.quote);
+        $(".blockquote-footer").html(json.author);
+      },
+      beforeSend:function(xhr){
+        xhr.setRequestHeader("X-Mashape-Key", "yjSdsNkGk6mshE03Fn4dyAjuMQZxp192cnojsnqsuHp5HksVsz");
+      }
+    });
+  }
+  randQuote();
+  $('#getQuote').click(function(){
+    randQuote();
     var randomColor='rgb(' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ',' + (Math.floor(Math.random() * 256)) + ')';
     $('body').css('background-color',randomColor);
     $('h1').css('color',randomColor);
@@ -19,8 +49,8 @@ $(document).ready(function() {
     $('#quote').css('color',randomColor);
     $('#author').css('color',randomColor);
     $('button').css('background-color',randomColor);
-  
-
   });
+  $('#twitter').click(function(){
+      window.open('https://twitter.com/intent/tweet?text='+ $('#quote').html() + $('.blockquote-footer').html());;
+    });
 });
-
